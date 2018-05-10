e2e33c20125f4d931977fbc9d540d01044df6a0f
Lucas Gustavo Sebold <42165@UNIDAVI.LOCAL>
2018-05-09 21:20:30 -0300

    Adicionando informacao na tabela produto

--- estoque/database/seeds/DatabaseSeeder.php
+++ estoque/database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +13,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(ProdutoTableSeeder::class);
+    }
+}
+
+class ProdutoTableSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      DB::insert('insert into produtos (nome, quantidade, valor, descricao) values (?,?,?,?)', array('Geladeira', 2, 110, 'Side by Side com gelo na porta'));
+      DB::insert('insert into produtos (nome, quantidade, valor, descricao) values (?,?,?,?)', array('Fogão', 5, 100, 'Painel automático e forno elétrico'));
+      DB::insert('insert into produtos (nome, quantidade, valor, descricao) values (?,?,?,?)', array('Microondas', 1, 152, 'Manda SMS quando termina de esquentar'));
     }
 }
